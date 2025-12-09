9147d5703ce13760ed5a64ce0645a1377f4a53a9
holdn2 <yoocy01@gmail.com>
2025-12-09 09:42:03 +0900

    init: initiailize project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef } from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import WebView from "react-native-webview";
+
+const WEB_URL = "https://ku-room.vercel.app";
 
 export default function App() {
+  const webViewRef = useRef<WebView | null>(null);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container} edges={["top"]}>
+        <WebView
+          ref={(ref) => {
+            webViewRef.current = ref;
+          }}
+          source={{ uri: WEB_URL }}
+          style={styles.webview}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f8f9fc",
+  },
+  webview: {
+    flex: 1,
+    backgroundColor: "#f8f9fc",
   },
 });
